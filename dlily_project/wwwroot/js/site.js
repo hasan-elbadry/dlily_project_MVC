0ced02dd2f51e612639f1e4da3b3266d69e487f2
hasan_elbadry <hasan.elbadry2006@gmail.com>
2025-02-09 18:28:11 +0200

    nav bar changes

--- dlily_project/wwwroot/js/site.js
+++ dlily_project/wwwroot/js/site.js
@@ -19,3 +19,26 @@
         }
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            // Remove active class from all links
+            navLinks.forEach(link => link.classList.remove('active'));
+
+            // Add active class to the clicked link
+            this.classList.add('active');
+        });
+    });
+
+    // Set the initial active link based on the current page
+    const currentUrl = window.location.href;
+    navLinks.forEach(link => {
+        if (link.href === currentUrl) {
+            link.classList.add('active');
+        }
+    });
+});
