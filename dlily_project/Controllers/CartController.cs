daa7352b7de24c41c41024ada009eae7b5eccbc0
MostafaBnMedhat <mostafamedhat577@gmail.com>
2025-02-17 20:10:13 +0200

    added payment in tourguide

---
+++ dlily_project/Controllers/CartController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace dlily_project.Controllers
+{
+    public class CartController : Controller
+    {
+        private static List<CartItem> cart = new List<CartItem>();
+         private readonly ApplicationDbContext _context;
+
+    public CartController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+        [HttpGet]
+        public JsonResult GetCart()
+        {
+            return Json(cart); 
+        }
+        public IActionResult AddToCart(int id)
+        {
+            var selectedGuide = _context.Tourgides.FirstOrDefault(g => g.Id == id);
+            if (selectedGuide == null)
+            {
+                return NotFound();
+            }
+
+          
+            cart.Add(new CartItem
+            {
+                Id = selectedGuide.Id,
+                Name = selectedGuide.Name,
+                Price = selectedGuide.Price
+            });
+
+            return RedirectToAction("Cart");
+        }
+
+        public IActionResult Cart()
+        {
+            return View(cart); 
+        }
+        
+     
+        public IActionResult Checkout()
+        {
+            double totalAmount = cart.Sum(item => item.Price);
+          
+            return View(totalAmount);
+        }
+    }
+
+}
---
+++ dlily_project/DAL/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace dlily_project.DAL.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+}
